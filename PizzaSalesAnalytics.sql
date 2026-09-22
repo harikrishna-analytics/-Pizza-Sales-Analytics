148460e704f78551df7a6ae7b9bb19f7d231045e
Hari Krishna <hanrajharikrishna@gmail.com>
2026-09-22 12:43:07 +0530

    Add files via upload

--- PizzaSalesAnalytics.sql
+++ PizzaSalesAnalytics.sql
@@ -1,342 +1,306 @@
-select * from Pizza_Sales;
-select count( cast(order_id as nvarchar(50))) as orders_count  from Pizza_Sales;
-select * from Pizza_Sales;
+use PizzaSalesAnalytics;
+
+========================== Daily Revenue ====================================================
 
 
-select
-pizza_category,
-count( cast(order_id as nvarchar(50))) as orders_count  
-from Pizza_Sales group by pizza_category  having       count(cast(order_id as nvarchar(50)))<  ( select count(cast(order_id as nvarchar(50)))  as total_orders 
-from Pizza_Sales)
-
-union all 
-
-select  pizza_category ,count(cast(order_id as nvarchar(50))) as orders 
-from Pizza_Sales
-group by pizza_category;
-
-
-
-
-select
-pizza_category,
-count( cast(order_id as nvarchar(50))) as orders_count  
-from Pizza_Sales group by pizza_category  having       count(cast(order_id as nvarchar(50)))<  ( select count(cast(order_id as nvarchar(50)))  as total_orders 
-from Pizza_Sales)
-
-union 
-
-select  pizza_category ,count(cast(order_id as nvarchar(50))) as orders 
-from Pizza_Sales
-group by pizza_category;
-
-
-
-
-SELECT
-    pizza_category,
-    COUNT(DISTINCT CAST(order_id AS NVARCHAR(50))) AS orders_count
-FROM Pizza_Sales
-GROUP BY pizza_category
-HAVING COUNT(DISTINCT CAST(order_id AS NVARCHAR(50))) >
+WITH DailyRevenue AS
 (
-    SELECT AVG(category_orders)
-    FROM
-    (
-        SELECT
-            pizza_category,
-            COUNT(DISTINCT CAST(order_id AS NVARCHAR(50))) AS category_orders
-        FROM Pizza_Sales
-        GROUP BY pizza_category
-    ) AS x
+    SELECT
+        CAST(order_date AS DATE) AS order_date,
+        SUM(total_price) AS daily_revenue
+    FROM Pizza_Sales
+    GROUP BY CAST(order_date AS DATE)
 )
-ORDER BY orders_count DESC;
-
-
-=====================================================================
---------- Total pizza sold------------------------
-
-select   pizza_category ,sum(quantity) as total_sold
- from pizza_sales  group by pizza_category
-
- ----------------------- Revenue by Pizza type ---------------
- 
-select   pizza_category ,    format(sum(quantity*total_price) ,'c','en_us')
-as total_revenue
- from pizza_sales  group by pizza_category order by total_revenue desc 
- ----------- Revenue and orders by pizza type --------------
-
- 
- 
-select   pizza_category ,    format(sum(quantity*total_price) ,'c','en_us')
-as total_revenue,count(distinct cast(order_id as nvarchar(50))) as total_aorders 
- from pizza_sales  group by pizza_category order by total_revenue desc ;
-
- -----------========= MOnthly pizza sold----------------------
-
- select datename(month, order_date) as month_name, count(distinct cast(order_id as nvarchar(50))) as total_orders 
-    from pizza_sales  group by datename(month,order_date) 
-     order by total_orders desc 
-
-
-    ============================p====================================================================================
-    ---------------- top 10 sales and orders  with pizza name -------------------
-
-
-    select top 10
-    pizza_name,
-    count(distinct cast(order_id as nvarchar(50))) as total_orders,
-    sum(quantity) as total_sold,
-    sum(quantity*total_price) as Revenue 
-    from pizza_sales
-    group by pizza_name 
-     order by Revenue desc 
-
-
-     ---------------- Bottom 10 sales and orders with pizza name ------------------
-
-      select top 10
-    pizza_name,
-    count(distinct cast(order_id as nvarchar(50))) as total_orders,
-    sum(quantity) as total_sold,
-    sum(quantity*total_price) as Revenue 
-    from pizza_sales
-    group by pizza_name 
-     order by Revenue asc ;
-
-
-     -----------------------------------------------------------------------------------------------
-     =====================================================================================================================================
-
-     ----------------- monthly pizza type sales ---------------------
-
-           select 
-           datename(month, order_date) as month_name,
-    pizza_name,
-    count(distinct cast(order_id as nvarchar(50))) as total_orders,
-    sum(quantity) as total_sold,
-    sum(quantity*total_price) as Revenue 
-    from pizza_sales
-    group by 
-    datename(month, order_date) ,
-    pizza_name 
-     order by Revenue asc ;
-     ===========================================================================================
-
-     --------------- top 10 saels of pizza monthly-----------------------------
-
-
-          select  top 10
-           datename(month, order_date) as month_name,
-    pizza_name,
-    count(distinct cast(order_id as nvarchar(50))) as total_orders,
-    sum(quantity) as total_sold,
-    sum(quantity*total_price) as Revenue 
-    from pizza_sales
-    group by 
-    datename(month, order_date) ,
-    pizza_name 
-     order by Revenue asc ;
-
-=========================================================================================
-
- ------------------- Timeline pizza sales -----------------------------
-
- select
-  year(order_date) as year_name,
-  month(order_date) as month_number,
-   datename(month,order_date) as month_name,
-     count(distinct cast(order_id as nvarchar(50))) as total_orders,
-    sum(quantity) as total_sold,
-    sum(quantity*total_price) as Revenue 
-    from pizza_sales
-
-    group by
-
-    year(order_date),
-    month(order_date),
-    datename(month,order_date)
-    order by Revenue desc 
-
-
-    ======================================================================================================
-       ----------- views------------
-
-
-   CREATE VIEW vw_Monthly_Pizza_Sales AS
 SELECT
-    YEAR(order_date) AS year_name,
-    MONTH(order_date) AS month_number,
-    DATENAME(MONTH, order_date) AS month_name,
-    COUNT(DISTINCT CAST(order_id AS NVARCHAR(50))) AS total_orders,
-    SUM(quantity) AS total_sold,
-    SUM(quantity * total_price) AS Revenue
-FROM Pizza_Sales
+    order_date,
+    daily_revenue,
+    AVG(daily_revenue) OVER
+    (
+        ORDER BY order_date
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) AS rolling_7_day_avg
+FROM DailyRevenue
+ORDER BY order_date;
+
+
+===================================================================================================================================
+
+=====------------------------------ Basket Analysis ---------------------------------------------
+
+   WITH CleanPizza AS
+(
+    SELECT
+        CAST(order_id AS NVARCHAR(100)) AS order_id,
+        CAST(pizza_name AS NVARCHAR(200)) AS pizza_name
+    FROM Pizza_Sales
+)
+SELECT
+    a.pizza_name AS pizza_1,
+    b.pizza_name AS pizza_2,
+    COUNT(DISTINCT a.order_id) AS orders_together
+FROM CleanPizza a
+INNER JOIN CleanPizza b
+    ON a.order_id = b.order_id
+   AND a.pizza_name < b.pizza_name
 GROUP BY
-    YEAR(order_date),
-    MONTH(order_date),
-    DATENAME(MONTH, order_date);
-
-    select  * from  vw_Monthly_Pizza_Sales ;
-
-    ============================================================================================
-
-    --------------- Stored Procedures ----------------------------
+    a.pizza_name,
+    b.pizza_name
+ORDER BY
+    orders_together DESC;
+    
 
 
-               SELECT
+    ==========================================================================================================
+
+    ----------------------- Top 3 pizza with pizza category -------------------------
+
+
+    WITH PizzaRevenue AS
+(
+    SELECT
+        pizza_category,
+        pizza_name,
+        SUM(total_price) AS revenue
+    FROM Pizza_Sales
+    GROUP BY
+        pizza_category,
+        pizza_name
+),
+RankedPizzas AS
+(
+    SELECT
+        pizza_category,
+        pizza_name,
+        revenue,
+        ROW_NUMBER() OVER
+        (
+            PARTITION BY pizza_category
+            ORDER BY revenue DESC
+        ) AS rn
+    FROM PizzaRevenue
+)
+SELECT
     pizza_category,
-    SUM(quantity) AS total_sold,
-    SUM(quantity * total_price) AS Revenue
-FROM Pizza_Sales
-GROUP BY pizza_category
-order by Revenue ;
+    pizza_name,
+    revenue
+FROM RankedPizzas
+WHERE rn <= 3
+ORDER BY
+    pizza_category,
+    revenue DESC;
 
 
-create  procedure sp_category_sales
-as 
 
- BEGIN 
+
+    select * from pizza_sales 
+
+ 
+ with PizzaRevenue as 
+(
+      select
+      pizza_category,
+      pizza_size ,
+      sum(quantity*total_price) as revenue ,
+      sum(quantity) as pizza_sold 
+       from pizza_sales
+        group by 
+        pizza_category,
+        pizza_size
+         )
+        ,  RankedPizza 
+        as ( 
+         select
+      pizza_category,
+      pizza_size ,
+      revenue,
+      row_number() over( partition by pizza_category order by revenue desc) as rn 
+       from PizzaRevenue)
+       select 
+       pizza_category,
+       pizza_size,
+       revenue 
+        from RankedPizza
+        where rn<= 3 
+        order by 
+        pizza_category,
+        revenue desc;
+
+
+
+        ====================================================================================================================================================
+
+
+
+        WITH MonthlyRevenue AS
+(
+    SELECT
+        YEAR(order_date) AS order_year,
+        MONTH(order_date) AS order_month,
+
+        SUM(total_price) AS revenue
+    FROM Pizza_Sales
+    GROUP BY
+        YEAR(order_date),
+        MONTH(order_date)
+),
+RevenueWithPrevious AS
+(
+    SELECT
+        order_year,
+        order_month,
+        revenue,
+        LAG(revenue) OVER
+        (
+            ORDER BY order_year, order_month
+        ) AS previous_month_revenue
+    FROM MonthlyRevenue
+)
+SELECT
+    order_year,
+    order_month,
+    revenue,
+    previous_month_revenue,
+    ROUND(
+        (revenue - previous_month_revenue)
+        * 100.0
+        / NULLIF(previous_month_revenue, 0),
+        2
+    ) AS mom_growth_pct
+FROM RevenueWithPrevious
+ORDER BY
+    order_year,
+    order_month
+ ;
+
+
+ ======================================================================================================================
+
+ ----------------------------- peak Hours Analysis ----------------------------------------
 
  SELECT
-    pizza_category,
-    SUM(quantity) AS total_sold,
-    SUM(quantity * total_price) AS Revenue
+    DATEPART(HOUR, order_time) AS order_hour,
+    COUNT(DISTINCT CAST(order_id AS NVARCHAR(50))) AS total_orders,
+    SUM(quantity) AS pizzas_sold,
+    SUM(quantity * total_price) AS revenue
 FROM Pizza_Sales
-GROUP BY pizza_category;
+GROUP BY
+    DATEPART(HOUR, order_time)
+ORDER BY
+    total_orders DESC;
 
- END  ;
+    =====================================================================================================================
+        
+   ----------------------------------------- Peak Days Analysis ---------------------------------
 
- EXEC sp_category_sales;
-  
+            select 
+            datename(Day,order_date) as order_day_number,
+            datename(weekday,order_date) as order_day,
+            count(distinct cast(order_id as nvarchar(50))) as total_orders,
+            sum(quantity) as pizzas_sold,
+            sum(quantity * total_price) as revenue
+            from pizza_sales
+            group by 
+             datename(day,order_date) ,
+              datename(weekday,order_date)
 
- =================================================================================================================================
- ----------------- MOnthly_Revenue BY Pizza Sales ------------------------
+              order by 
+              total_orders desc 
+       ====================================================================================================
+       
+            select 
+            datename(Day,order_date) as order_day_number,
+            datename(weekday,order_date) as order_day,
+            count(distinct cast(order_id as nvarchar(50))) as total_orders,
+            sum(quantity) as pizzas_sold,
+            sum(quantity * total_price) as revenue
+            from pizza_sales
+            group by 
+             datename(day,order_date) ,
+              datename(weekday,order_date)
 
- select 
-  datename(month,order_date) as month_name,
-  pizza_category,
-  sum(quantity * total_price) as Revenue,
-  sum(quantity) as total_sold 
-   from pizza_sales 
-   group  by
-   datename(month,order_date),
-   pizza_category
-    order by Revenue desc 
+                 having sum(quantity) <=1000
+              order by 
+              total_orders desc 
+           =====================================================================================================
 
+           SELECT 
+    DATENAME(DAY, order_date) AS order_day_number,
+    DATENAME(WEEKDAY, order_date) AS order_day,
+    COUNT(DISTINCT CAST(order_id AS NVARCHAR(50))) AS total_orders,
+    SUM(quantity) AS pizzas_sold,
+    SUM(quantity * total_price) AS revenue
+FROM Pizza_Sales
+GROUP BY 
+    DATENAME(DAY, order_date),
+    DATENAME(WEEKDAY, order_date)
+HAVING 
+    SUM(quantity) <= 1000
+ORDER BY 
+    total_orders DESC;
 
-    -------------------------- sp_Monthly_Pizza_Sales --------------------------------------
+    ====================================================================================================================================
 
-    create procedure sp_Monthly_Pizza_Sales 
-    as 
-    begin 
-    select 
-  datename(month,order_date) as month_name,
-  pizza_category,
-  sum(quantity * total_price) as Revenue,
-  sum(quantity) as total_sold 
-   from pizza_sales 
-   group  by
-   datename(month,order_date),
-   pizza_category
-    order by Revenue desc 
-end;
+    SELECT 
+    DATEPART(WEEKDAY, order_date) AS order_day_number,
+    DATENAME(WEEKDAY, order_date) AS order_day,
+    COUNT(DISTINCT CAST(order_id AS NVARCHAR(50))) AS total_orders,
+    SUM(quantity) AS pizzas_sold,
+    SUM(quantity * total_price) AS revenue
+FROM Pizza_Sales
+GROUP BY 
+    DATEPART(WEEKDAY, order_date),
+    DATENAME(WEEKDAY, order_date)
+HAVING 
+    SUM(quantity) > =500
+ORDER BY 
+    total_orders DESC;
 
-exec sp_Monthly_Pizza_Sales ;
-
-====================================================================================================
-
----------------------------- year wise and pizza type  wise sales -------------
-
-  select top 10
-   year(order_date) as year_name,
-   pizza_name ,
-   sum(quantity*total_price)as  Revenue,
-   sum(quantity) as total_sold
-    from pizza_sales
-     group by 
-     year(order_date),
-     pizza_name
-      order by Revenue desc 
-      ======================================================================================
-
-      -------------------- sp_Year_Pizza_Sales ------------------------------------------
-
-      
-
-      create procedure    sp_Year_Pizza_Sales 
-      as 
-       begin 
-  select top 10
-   year(order_date) as year_name,
-   pizza_name ,
-   sum(quantity*total_price)as  Revenue,
-   sum(quantity) as total_sold
-    from pizza_sales
-     group by 
-     year(order_date),
-     pizza_name
-      order by Revenue desc 
-
-      end 
-
-      exec sp_Year_Pizza_Sales 
-      ==================================================================
+==================================================================================================================================
 
 
 
-      CREATE PROCEDURE sp_Monthly_Pizza_Sales_By_Year
-    @Year INT
-AS
-BEGIN
+SELECT 
+    DATEPART(WEEKDAY, order_date) AS order_day_number,
+    DATENAME(WEEKDAY, order_date) AS order_day,
+    COUNT(DISTINCT CAST(order_id AS NVARCHAR(50))) AS total_orders,
+    SUM(quantity) AS pizzas_sold,
+    SUM(quantity * total_price) AS revenue
+FROM Pizza_Sales
+GROUP BY 
+    DATEPART(WEEKDAY, order_date),
+    DATENAME(WEEKDAY, order_date)
+HAVING 
+    SUM(quantity) >= 500
+    or  SUM(quantity) <= 1000
+ORDER BY 
+    total_orders DESC;
 
-    SELECT
-        DATENAME(MONTH, order_date) AS month_name,
-        pizza_category,
-        SUM(quantity * total_price) AS Revenue,
-        SUM(quantity) AS total_sold
-    FROM Pizza_Sales
-    WHERE YEAR(order_date) = @Year
-    GROUP BY
-        DATENAME(MONTH, order_date),
-        pizza_category
-    ORDER BY Revenue DESC;
 
-END;
+    ======================================================================================================================================
 
-exec sp_Monthly_Pizza_Sales_By_Year;
-===============================================
-use PizzaSalesAnalytics
+    
+ select * from pizza_sales
 
-select  pizza_category , sum(quantity*total_price) as Revenue,
-sum(quantity) as total_sold,
-count(distinct cast( order_id as nvarchar(50))) as total_orders 
-from pizza_sales
 
+ SELECT DISTINCT pizza_category,
+ count(distinct cast(order_id as nvarchar(50))) as total_orders
+FROM Pizza_Sales
 group by pizza_category
-order by  2 desc 
+ORDER BY total_orders desc;
+
+========================================================================
 
 
-SELECT   pizza_category,
-         format(sum(quantity * total_price), 'c', 'en_us') AS Revenue,
-         sum(quantity) AS total_sold,
-         count(DISTINCT CAST (order_id AS NVARCHAR (50))) AS total_orders
-FROM     pizza_sales
+SELECT 
+    pizza_category,
+    COUNT(DISTINCT CAST(order_id AS NVARCHAR(50))) AS total_orders,
+    SUM(quantity) AS pizzas_sold,
+    SUM(quantity * total_price) AS revenue
+FROM Pizza_Sales
 GROUP BY pizza_category
-ORDER BY 2 DESC; 
+ORDER BY revenue DESC;
 
 
-select pizza_name,
- format(sum(quantity * total_price), 'c', 'en_us') AS Revenue,
-dense_rank()
-over(  partition 
+=====================================================================================================================
 
 
-
-
-
-select pizza_category,
-format (round(sum(quantity*total_price),2),'c','en_us') as revenue 
- from pizza_sales 
- group by pizza_categoryby pizza_name order by sum(quantity * total_price) desc)  as  revenue_rank
-from pizza_sales 
